bc3cb60cfa537493d15a6c6aa25d9c24bed68991
MasterchiefCC <chenchenstudent@gmail.com>
2018-07-23 15:03:24 +0800

    2018.5.23

--- bindings/python/wrap_json.c
+++ bindings/python/wrap_json.c
@@ -25,71 +25,71 @@
  * SUCH DAMAGE.
  */
 
-#include "../../json.c"
+#include "../json.c"
 
-PyObject * json_exception = PyErr_NewException("jsonparser.JSONException", 
-    NULL, NULL);
+PyObject* json_exception =
+  PyErr_NewException("jsonparser.JSONException", NULL, NULL);
 
-PyObject * get_exception_class()
+PyObject*
+get_exception_class()
 {
-    return json_exception;
+  return json_exception;
 }
 
-PyObject * convert_value(json_value * data)
+PyObject*
+convert_value(json_value* data)
 {
-    PyObject * value;
-    switch (data->type) {
-        case json_object:
-            value = PyDict_New();
-            for (int i = 0; i < data->u.object.length; i++) {
-                PyObject * name = PyUnicode_FromString(
-                    data->u.object.values[i].name);
-                PyObject * object_value = convert_value(
-                    data->u.object.values[i].value);
-                PyDict_SetItem(value, name, object_value);
-            }
-            break;
-        case json_array:
-            value = PyList_New(0);
-            for (int i = 0; i < data->u.array.length; i++) {
-                PyObject * array_value = convert_value(
-                    data->u.array.values[i]);
-                PyList_Append(value, array_value);
-            }
-            break;
-        case json_integer:
-            value = PyInt_FromLong(data->u.integer);
-            break;
-        case json_double:
-            value = PyFloat_FromDouble(data->u.dbl);
-            break;
-        case json_string:
-            value = PyUnicode_FromStringAndSize(data->u.string.ptr, 
-                data->u.string.length);
-            break;
-        case json_boolean:
-            value = PyBool_FromLong((long)data->u.boolean);
-            break;
-        default:
-            // covers json_null, json_none
-            Py_INCREF(Py_None);
-            value = Py_None;
-            break;
-    }
-    return value;
+  PyObject* value;
+  switch (data->type) {
+    case json_object:
+      value = PyDict_New();
+      for (int i = 0; i < data->u.object.length; i++) {
+        PyObject* name = PyUnicode_FromString(data->u.object.values[i].name);
+        PyObject* object_value = convert_value(data->u.object.values[i].value);
+        PyDict_SetItem(value, name, object_value);
+      }
+      break;
+    case json_array:
+      value = PyList_New(0);
+      for (int i = 0; i < data->u.array.length; i++) {
+        PyObject* array_value = convert_value(data->u.array.values[i]);
+        PyList_Append(value, array_value);
+      }
+      break;
+    case json_integer:
+      value = PyInt_FromLong(data->u.integer);
+      break;
+    case json_double:
+      value = PyFloat_FromDouble(data->u.dbl);
+      break;
+    case json_string:
+      value =
+        PyUnicode_FromStringAndSize(data->u.string.ptr, data->u.string.length);
+      break;
+    case json_boolean:
+      value = PyBool_FromLong((long)data->u.boolean);
+      break;
+    default:
+      // covers json_null, json_none
+      Py_INCREF(Py_None);
+      value = Py_None;
+      break;
+  }
+  return value;
 }
 
-PyObject * decode_json(char * data)
+PyObject*
+decode_json(char* data)
 {
-    json_settings settings;
-    memset(&settings, 0, sizeof (json_settings)); 
-    settings.settings = json_enable_comments;
-    char error[256];
-    json_value * value = json_parse_ex(&settings, data, strlen(data), error);
-    if (value == 0) {
-        return PyErr_Format(json_exception, error);
-    }
-    PyObject * converted = convert_value(value);
-    json_value_free(value);
-    return converted;
+  json_settings settings;
+  memset(&settings, 0, sizeof(json_settings));
+  settings.settings = json_enable_comments;
+  char error[256];
+  json_value* value = json_parse_ex(&settings, data, strlen(data), error);
+  if (value == 0) {
+    return PyErr_Format(json_exception, error);
+  }
+  PyObject* converted = convert_value(value);
+  json_value_free(value);
+  return converted;
 }
